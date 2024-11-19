bba6be617a10b9f36fb7435b723882c52de38e63
SagunNakarmi <sagun@qitplus.com>
2024-11-19 10:36:49 +1000

    Update comment

--- Services/InterpreterService.cs
+++ Services/InterpreterService.cs
@@ -13,7 +13,7 @@ public class InterpreterService : IInterpreterService
 
     public string InterpretNumberToWords(decimal inputNumber, int mode)
     {
-        // Assuming the range of input numbers to be from 0.00 to 9,999,999.99 .
+        // Assuming the range of input numbers to be from 0.00 to 9,999,999,999,999,999.99 .
         // I'm also setting a premise that the numbers are to be interpreted as currency, as it was shown in the example.
         // We will throw an error when the value is not within the above range
 
